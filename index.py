bf81567b0e67bf82de199a5142e9faff45d8ec0e
Mohamed Salman <salman@myheathcare.life>
2021-05-05 17:24:34 +0530

    Reverse polish notation calculator

---
+++ index.py
@@ -0,0 +1,81 @@
+import sys
+import re
+
+
+class RpnCalculator:
+
+    def __init__(self):
+        self.all_numbers = []
+        self.all_operators = []
+        self.allowed_operators = ['+', '-', '*', '/']
+        self.exit_statements = ['q', 'quit', 'exit']
+        self.invalid_calculation = (
+            "Numbers not available to process calculation, " +
+            "try inputting a number first"
+        )
+        self.empty_input = "Please enter a number or an operator(+ - * /)"
+
+    def calculate(self, user_input):
+        all_numbers_count = len(self.all_numbers)
+        if len(self.all_operators) > 0:
+            for idx, operator in enumerate(self.all_operators):
+                try:
+                    last = float(self.all_numbers[-1])
+                except IndexError:
+                    continue
+
+                try:
+                    second_last = float(self.all_numbers[-2])
+                except IndexError:
+                    continue
+
+                if operator == '+':
+                    value = second_last + last
+                if operator == '-':
+                    value = second_last - last
+                if operator == '*':
+                    value = second_last * last
+                if operator == '/':
+                    value = second_last / last
+
+                self.all_numbers.pop()
+                self.all_numbers.pop()
+                self.all_numbers.append(str(value))
+
+            self.all_operators = []
+
+        if all_numbers_count <= 1 and user_input in self.allowed_operators:
+            print(self.invalid_calculation)
+            self.get_input()
+            return
+
+        return self.all_numbers[-1]
+
+    def get_input(self):
+        user_input = input("> ")
+        if user_input == '' or user_input is None:
+            print(self.empty_input)
+            self.get_input()
+            return
+        if (user_input in self.exit_statements):
+            sys.exit()
+
+        parsed_input = user_input.split(sep=" ")
+        parsed_input = list(filter(None, parsed_input))
+
+        for element in parsed_input:
+            if element in self.allowed_operators:
+                self.all_operators.append(element)
+                continue
+            if re.match(r'^(?=.)([+-]?([0-9]*)(\.([0-9]+))?)$', element):
+                self.all_numbers.append(element)
+
+        output = self.calculate(user_input)
+        print(output)
+
+        self.get_input()
+        return
+
+
+objRpn = RpnCalculator()
+objRpn.get_input()
---
+++ readme.md
@@ -0,0 +1,68 @@
+CLI RPN Calculator
+==================
+
+Command-line reverse polish notation (RPN) calculator using Python 3.
+I have built this command-line calculator for people who are comfortable with UNIX-like CLI utilities.
+
+Specifications
+--------------
+
+1. The calculator uses standard input and standard output
+2. It implements the four standard arithmetic operators i.e. + - * /
+3. The calculator handle errors and recovers gracefully
+4. The calculator exits when it receives a `q`, `quit`, `exit` command
+
+How to run code
+------
+
+* Install Python 3 on your PC
+* Clone repository on your local PC
+* Open a Terminal or Command Prompt
+* In terminal, go to the cloned directory 
+* Check Python 3 runs using `python` or  `python3` command
+* In your cloned directory, run this command from terminal - `python3 index.py`
+
+Example Input/Output
+--------------------
+
+This RPN calculator have been tested with following inputs and gives the expected results
+
+    > 5 
+    5
+    > 8
+    8
+    > +
+    13
+
+---
+
+    > 5 5 5 8 + + -
+    -13.0
+    > 13 +
+    0.0
+
+---
+
+    > -3
+    -3.0
+    > -2
+    -2.0
+    > *
+    6.0
+    > 5
+    5.0
+    > +
+    11.0
+
+---
+
+    > 5
+    5
+    > 9
+    9
+    > 1
+    1
+    > -
+    8
+    > /
+    0.625
